4dff9954af37f5c40dfb9dd0c0f6e37151d6dab9
Corentin Le Gall <corentin.le_gall@outlook.fr>
2022-05-16 09:50:20 +0200

    Minor corrections

--- gan/common.py
+++ gan/common.py
@@ -1,5 +1,5 @@
 ##########################################################
-#                         Imports                        #
+#						 Imports						#
 ##########################################################
 from tensorflow.keras import layers
 import tensorflow as tf
@@ -34,14 +34,14 @@ def downsample(filters, size, apply_batchnorm=True):
 	initializer = tf.random_normal_initializer(0., 0.02)
 	
 	result = tf.keras.Sequential()
-  	result.add(
-    	tf.keras.layers.Conv2D(
-    		filters, 
-    		size, 
-    		strides=2, 
-    		padding='same',
-         	kernel_initializer=initializer, 
-         	use_bias=False
+	result.add(
+		tf.keras.layers.Conv2D(
+			filters, 
+			size, 
+			strides=2, 
+			padding='same',
+		 	kernel_initializer=initializer, 
+		 	use_bias=False
 		)
 	)
 	
@@ -56,10 +56,10 @@ def upsample(filters, size, apply_dropout=False):
 	initializer = tf.random_normal_initializer(0., 0.02)
   	result = tf.keras.Sequential()
   	result.add(
-    	tf.keras.layers.Conv2DTranspose(
-    		filters, 
-    		size, 
-    		strides=2,
+		tf.keras.layers.Conv2DTranspose(
+			filters, 
+			size, 
+			strides=2,
 			padding='same',
 			kernel_initializer=initializer,
 			use_bias=False
@@ -78,7 +78,7 @@ def upsample(filters, size, apply_dropout=False):
 def generate_images(model, test_input, tar):
 	prediction = model(test_input, training=True)
 	plt.figure(figsize=(15, 15))
-
+	files = os.listdir(r"./info/trainingImages/")
 	display_list = [test_input[0], tar[0], prediction[0]]
 	title = ['Input Image', 'Ground Truth', 'Predicted Image']
 
@@ -88,7 +88,7 @@ def generate_images(model, test_input, tar):
 		# Getting the pixel values in the [0, 1] range to plot.
 		plt.imshow(display_list[i] * 0.5 + 0.5)
 		plt.axis('off')
-	plt.show()
+	plt.savefig(fr"./info/trainingImages/{len(files)}.png")
 
 
 OUTPUT_CHANNELS = 3
--- gan/generator.py
+++ gan/generator.py
@@ -1,7 +1,7 @@
 # This File contain the class of the generator part of the Generative Adversarial Network
 
 ##########################################################
-#                         Imports                        #
+#						 Imports						#
 ##########################################################
 from tensorflow.keras import layers
 import tensorflow as tf
@@ -19,24 +19,24 @@ def Generator():
   	inputs = tf.keras.layers.Input(shape=[256, 256, 3])
 	
   	down_stack = [
-    	downsample(64, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
-    	downsample(128, 4),  # (batch_size, 64, 64, 128)
-    	downsample(256, 4),  # (batch_size, 32, 32, 256)
-    	downsample(512, 4),  # (batch_size, 16, 16, 512)
-    	downsample(512, 4),  # (batch_size, 8, 8, 512)
-    	downsample(512, 4),  # (batch_size, 4, 4, 512)
-    	downsample(512, 4),  # (batch_size, 2, 2, 512)
-    	downsample(512, 4),  # (batch_size, 1, 1, 512)
+		downsample(64, 4, apply_batchnorm=False),  # (batch_size, 128, 128, 64)
+		downsample(128, 4),  # (batch_size, 64, 64, 128)
+		downsample(256, 4),  # (batch_size, 32, 32, 256)
+		downsample(512, 4),  # (batch_size, 16, 16, 512)
+		downsample(512, 4),  # (batch_size, 8, 8, 512)
+		downsample(512, 4),  # (batch_size, 4, 4, 512)
+		downsample(512, 4),  # (batch_size, 2, 2, 512)
+		downsample(512, 4),  # (batch_size, 1, 1, 512)
   	]
 
   	up_stack = [
-    	upsample(512, 4, apply_dropout=True),  # (batch_size, 2, 2, 1024)
-    	upsample(512, 4, apply_dropout=True),  # (batch_size, 4, 4, 1024)
-    	upsample(512, 4, apply_dropout=True),  # (batch_size, 8, 8, 1024)
-    	upsample(512, 4),  # (batch_size, 16, 16, 1024)
-    	upsample(256, 4),  # (batch_size, 32, 32, 512)
-    	upsample(128, 4),  # (batch_size, 64, 64, 256)
-    	upsample(64, 4),  # (batch_size, 128, 128, 128)
+		upsample(512, 4, apply_dropout=True),  # (batch_size, 2, 2, 1024)
+		upsample(512, 4, apply_dropout=True),  # (batch_size, 4, 4, 1024)
+		upsample(512, 4, apply_dropout=True),  # (batch_size, 8, 8, 1024)
+		upsample(512, 4),  # (batch_size, 16, 16, 1024)
+		upsample(256, 4),  # (batch_size, 32, 32, 512)
+		upsample(128, 4),  # (batch_size, 64, 64, 256)
+		upsample(64, 4),  # (batch_size, 128, 128, 128)
   	]
 
   	initializer = tf.random_normal_initializer(0., 0.02)
@@ -54,15 +54,15 @@ def Generator():
   	# Downsampling through the model
   	skips = []
   	for down in down_stack:
-    	x = down(x)
-    	skips.append(x)
+		x = down(x)
+		skips.append(x)
 	
   	skips = reversed(skips[:-1])
 	
   	# Upsampling and establishing the skip connections
   	for up, skip in zip(up_stack, skips):
   	  	x = up(x)
-    	x = tf.keras.layers.Concatenate()([x, skip])
+		x = tf.keras.layers.Concatenate()([x, skip])
 
   	x = last(x)
 
